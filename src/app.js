c2549da7d6441f92068aa62763d3c6a622453124
CMDRyan <cmd.ryanm@gmail.com>
2021-07-28 15:24:08 +0800

    Add filter to app.js

--- src/app.js
+++ src/app.js
@@ -8,10 +8,17 @@ app.use(express.static(path.join(__dirname)));
 
 app.set("view engine", "ejs");
 
+//app.get("redirect", (req,res) => res.render("pages/redirect"))
+
 app.get("/groups/:id/jobs", function (req, res) {
-  getJobs
-    .getJobs(req.params.id)
-    .then((data) => res.render("pages/index", { data: data }));
+  getJobs.getJobs(req.params.id).then((data) =>
+    res.render("pages/index", {
+      created: data.filter((data) => data.status === "created"),
+      pending: data.filter((data) => data.status === "pending"),
+      running: data.filter((data) => data.status === "running"),
+    })
+  );
+  // .then((data) => console.log((data[0]).map(({status, ...rest})=> [status[1], rest])));
 });
 
 app.listen(port, () => {
