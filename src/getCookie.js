c9523bc81fef0d7ab0c1d9b2c9f33e18ebbccf35
CMDRyan <cmd.ryanm@gmail.com>
2021-07-30 18:42:59 +0800

    Include case undefined

--- src/getCookie.js
+++ src/getCookie.js
@@ -1,6 +1,6 @@
 exports.getCookie = function getTokenCookie(req){
         let cookie = decodeURIComponent(req.headers.cookie);
-        if(cookie === ""){
+        if(cookie === ""|| cookie === `access_token=undefined`){
             return 0
         }
         else{
