d271d57bc22892f00fc27311621fe18f596486f2
CMDRyan <cmd.ryanm@gmail.com>
2021-08-04 18:01:37 +0800

    Add cookie maximum age to config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
-//const env = process.env.NODE_ENV; // 'dev' or 'test'
-
 const config = {
   host: `http://localhost:8081`,
   port: 8081,
   AppID: `7c0c8e717c744b103b263ef8b89d17cc429e6cfab0cddb8b3df21b66e21b45c1`,
   APP_SECRET: `${process.env.APP_SECRET}`,
   redirect_url: `http://localhost:8081/redirect`,
+  cookieAge: 86400000,
 };
 
 module.exports = config;
