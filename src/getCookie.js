4e7182c547fe9c5989cd924f58087e5b42aa4bce
CMDRyan <cmd.ryanm@gmail.com>
2021-08-04 18:01:54 +0800

    Remove getCookie.js

--- src/getCookie.js
+++
@@ -1,9 +0,0 @@
-exports.getCookie = function getTokenCookie(req) {
-  let cookie = decodeURIComponent(req.headers.cookie);
-  if (cookie === "" || cookie === `access_token=undefined`) {
-    return 0;
-  } else {
-    const token = cookie.split("=");
-    return token[1];
-  }
-};
