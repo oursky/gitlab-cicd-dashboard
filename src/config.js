899b884878449f99c618db3d46af5fd1bd9cf03e
CMDRyan <cmd.ryanm@gmail.com>
2021-08-05 17:21:04 +0800

    Remove port config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 const config = {
   origin: `${process.env.APP_ORIGIN}`,
-  port: `${process.env.APP_PORT}`,
   AppID: `${process.env.APP_ID}`,
   APP_SECRET: `${process.env.APP_SECRET}`,
   redirect_url: `${process.env.APP_ORIGIN}/redirect`,
-  cookieAge: 86400000,
+  cookieAge: `${process.env.COOKIE_AGE}`,
 };
 
 module.exports = config;
