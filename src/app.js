71334936a12deb3e4ece28ed608efeae86405d4b
CMDRyan <cmd.ryanm@gmail.com>
2021-08-10 16:54:23 +0800

    Esculate to warning level and set default port

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const URL = require("url").URL;
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 const getJobs = require("./getJobs");
 const getToken = require("./getToken");
 
@@ -10,11 +11,13 @@ const config = require("./config");
 
 const app = express();
 
-const { origin, AppID, APP_SECRET, redirect_url, cookieAge } = config;
-if([origin, AppID, APP_SECRET, redirect_url, cookieAge].includes("")){
-  console.log("[WARNING] config.js has at least one empty property.")
+const { origin, AppID, APP_SECRET, redirect_url, cookieAge, DB_URL } = config;
+if ([origin, AppID, APP_SECRET, redirect_url, cookieAge].includes("")) {
+  console.warn("[WARNING] config.js has at least one empty property.");
 }
-const port = new URL(origin).port;
+if (!DB_URL) {
+  console.log("[INFO] DB_URL is not provided, the session will not be logged.");
+}
 
 app.use(express.static(path.join(__dirname)));
 app.set("views", __dirname + "/views");
@@ -22,6 +25,17 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
+
+if (!DB_URL) {
+  mongoose
+    .connect(`${DB_URL}`, { useNewUrlParser: true })
+    .then(() => console.log("[INFO] MongoDB Connected."))
+    .catch((err) => {
+      console.warn(
+        `[WARNING] MongoDB connection failed, logging feature will be disabled.`
+      );
+    });
+}
 
 app.get("/redirect/:state", (req, res) => {
   res.redirect(
@@ -52,11 +66,21 @@ app.get("/redirect", (req, res) => {
     );
 });
 
+const LogItem = require("./models/LogItem");
+
 app.get("/groups/:id/jobs", function (req, res) {
   if (req.cookies.access_token === "" || req.cookies.access_token == null) {
     res.redirect(`${origin}/redirect/` + encodeURIComponent(req.originalUrl));
     return;
   }
+
+  if (!DB_URL) {
+    const newLog = new LogItem({
+      request: encodeURIComponent(req.originalUrl),
+    });
+    newLog.save().then((log) => console.log(log));
+  }
+
   getJobs
     .getJobs(req.params.id, req.cookies.access_token)
     .then((data) =>
@@ -77,6 +101,6 @@ app.get("/error", (req, res) => {
   res.render("pages/error");
 });
 
-app.listen(port, () => {
-  console.log(`listening at ${origin}`);
+app.listen(new URL(origin).port || 8081, () => {
+  console.log(`[INFO] listening at ${origin}`);
 });
