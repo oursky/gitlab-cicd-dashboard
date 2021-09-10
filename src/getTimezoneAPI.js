041899aebe158f46dbfa63542cd04edebb19fd1d
CMDRyan <cmd.ryanm@gmail.com>
2021-09-10 15:47:03 +0800

    Change returns data as timezone

--- src/getTimezoneAPI.js
+++ src/getTimezoneAPI.js
@@ -2,5 +2,5 @@ const fetch = require("node-fetch");
 
 exports.getTimezone = function getTimezone(clientIP) {
   const clientInfo = fetch(`https://ipapi.co/${clientIP}/json`);
-  return clientInfo.utc_offset || "+0800"; //Default : Hong Kong
+  return clientInfo.timezone || "Asia/Hong_Kong"; //Default
 };
