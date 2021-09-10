17ee38a2aea52d6475e7c49e8aeb1523c188963f
CMDRyan <cmd.ryanm@gmail.com>
2021-09-10 17:16:36 +0800

    Remove time conversion handling in getData.js

--- src/getData.js
+++ src/getData.js
@@ -1,4 +1,5 @@
 const gitlabAPI = require("./gitlabAPI");
+const getLocalTime = require("./getLocalTime");
 
 module.exports.getProjectIDs = function getProjectIDs(requestedGroupID, token) {
   const apiToken = token;
@@ -29,39 +30,46 @@ module.exports.getJobs = function getJobs(
   requestedGroupID,
   token,
   projects,
-  timezoneOffset
+  timezone
 ) {
   const jobPromises = projects.map((project) => {
     return gitlabAPI.getJobsByProjectID(token, project.id).then((jobs) => {
       return jobs.map((job) => {
-        const zuluCreatedTime = new Date(job.created_at.replace("Z", ""));
-        const localCreatedTime = new Date(
-          zuluCreatedTime.getTime() + parseInt(timezoneOffset) * 36000
+        const createdTimeStr = getLocalTime.getTimeByTimezone(
+          job.created_at,
+          timezone
         );
-        const createdMonth = localCreatedTime.getMonth() + 1;
-        const createdTimeStr =
-          `${localCreatedTime.getFullYear()}` +
-          `/${createdMonth}` +
-          `/${localCreatedTime.getHours()}` +
-          ` ${localCreatedTime.getHours()}` +
-          `:${localCreatedTime.getMinutes()}` +
-          `:${localCreatedTime.getSeconds()}`;
+        const startedTimeStr =
+          getLocalTime.getTimeByTimezone(job.started_at, timezone) ||
+          "Not yet started";
+        // const zuluCreatedTime = new Date(job.created_at.replace("Z", ""));
+        // const localCreatedTime = new Date(
+        //   zuluCreatedTime.getTime() + parseInt(timezoneOffset) * 36000
+        // );
+        // const createdMonth = localCreatedTime.getMonth() + 1;
+        // const createdTimeStr =
+        //   `${localCreatedTime.getFullYear()}` +
+        //   `/${createdMonth}` +
+        //   `/${localCreatedTime.getHours()}` +
+        //   ` ${localCreatedTime.getHours()}` +
+        //   `:${localCreatedTime.getMinutes()}` +
+        //   `:${localCreatedTime.getSeconds()}`;
 
-        var startedTimeStr = "Not yet started";
-        if (job.started_at != null) {
-          const zuluStartedTime = new Date(job.started_at.replace("Z", ""));
-          const localStartedTime = new Date(
-            zuluStartedTime.getTime() + parseInt(timezoneOffset) * 36000
-          );
-          const startedMonth = localStartedTime.getMonth() + 1;
-          startedTimeStr =
-            `${localStartedTime.getFullYear()}` +
-            `/${startedMonth}` +
-            `/${localStartedTime.getHours()}` +
-            ` ${localStartedTime.getHours()}` +
-            `:${localStartedTime.getMinutes()}` +
-            `:${localStartedTime.getSeconds()}`;
-        }
+        // var startedTimeStr = "Not yet started";
+        // if (job.started_at != null) {
+        //   const zuluStartedTime = new Date(job.started_at.replace("Z", ""));
+        //   const localStartedTime = new Date(
+        //     zuluStartedTime.getTime() + parseInt(timezoneOffset) * 36000
+        //   );
+        //   const startedMonth = localStartedTime.getMonth() + 1;
+        //   startedTimeStr =
+        //     `${localStartedTime.getFullYear()}` +
+        //     `/${startedMonth}` +
+        //     `/${localStartedTime.getHours()}` +
+        //     ` ${localStartedTime.getHours()}` +
+        //     `:${localStartedTime.getMinutes()}` +
+        //     `:${localStartedTime.getSeconds()}`;
+        // }
         return {
           ...job,
           project_name: project.name,
