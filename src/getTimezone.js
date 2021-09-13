0ad6cfc1cca4d76fdbeb2084cdec38a10a8bc389
CMDRyan <cmd.ryanm@gmail.com>
2021-09-13 17:54:07 +0800

    Remove geoip-lite library

--- src/getTimezone.js
+++
@@ -1,9 +0,0 @@
-const geoip = require('geoip-lite');
-
-exports.getTimezone = function getTimezone(clientIP) {
-  if(clientIP == null){
-    return "Asia/Hong_Kong"; //default
-  }
-  const geoInfo = geoip.lookup(clientIP);
-  return geoInfo.timezone
-}
