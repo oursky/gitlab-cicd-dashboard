f9946195f31c0d5421e457d50d93df224059805e
CMDRyan <cmd.ryanm@gmail.com>
2021-09-13 14:00:44 +0800

    Rename getTimezone js

--- src/getTimezoneAPI.js
+++
@@ -1,6 +0,0 @@
-const fetch = require("node-fetch");
-
-exports.getTimezone = function getTimezone(clientIP) {
-  const clientInfo = fetch(`https://ipapi.co/${clientIP}/json`);
-  return clientInfo.timezone || "Asia/Hong_Kong"; //Default
-};
