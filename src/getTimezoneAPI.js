8399e4b10485e613492345a1bb6721c96641834b
CMDRyan <cmd.ryanm@gmail.com>
2021-09-14 12:05:31 +0800

    Change hardcoded IP

--- src/getTimezoneAPI.js
+++ src/getTimezoneAPI.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 exports.getTimezone = function getTimezone(clientIP){
-  const clientPromise = fetch(`https://ipapi.co/61.92.7.109/json/`)
+  const clientPromise = fetch(`https://ipapi.co/${clientIP}/json/`)
   return clientPromise.then((response)=>{
     const clientInfo = response.json()
     return clientInfo
