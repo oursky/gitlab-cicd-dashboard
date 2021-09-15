697f1992a1082b02073bbcad0ad3c757fe4ee0a7
CMDRyan <cmd.ryanm@gmail.com>
2021-09-15 15:20:24 +0800

    Format codes and remove comments

--- src/getLocalTime.js
+++ src/getLocalTime.js
@@ -1,32 +1,29 @@
 const { DateTime } = require("luxon");
 
-exports.getTimeByTimezone = function getTimeByTimezone(zuluTime, clientTimezone) {
+exports.getTimeByTimezone = function getTimeByTimezone(
+  zuluTime,
+  clientTimezone
+) {
   if (zuluTime == null) {
     return;
   }
-  let timezone = "Asia/Hong_Kong"
-  if(!clientTimezone.error){
-    timezone = clientTimezone.timezone
+  let timezone = "Asia/Hong_Kong";
+  if (!clientTimezone.error) {
+    timezone = clientTimezone.timezone;
   }
-  
+
   const timeObj = DateTime.fromISO(zuluTime);
-  console.log("API Error: "+clientTimezone.error)
-  console.log("Original Time: "+zuluTime)
-  console.log("Library converted DateTime obj: "+timeObj)
-  console.log("Current Timezone: "+timezone)
-  // const convertedTime = DateTime.fromObject(
-  //   {
-  //     year: timeObj.year,
-  //     month: timeObj.month,
-  //     day: timeObj.day,
-  //     hour: timeObj.hour,
-  //     minute: timeObj.minute,
-  //     second: timeObj.second,
-  //   },
-  //   { zone: timezone }
-  // );
-  const convertedTime = timeObj.setZone(timezone)
-  console.log("converted Time Obj: "+convertedTime.toString())
-  console.log("FINAL RESULT: Timezone: "+ timezone + " Time: " + convertedTime.toLocaleString(DateTime.DATETIME_MED))
+  console.log("API Error: " + clientTimezone.error);
+  console.log("Original Time: " + zuluTime);
+  console.log("Library converted DateTime obj: " + timeObj);
+  console.log("Current Timezone: " + timezone);
+  const convertedTime = timeObj.setZone(timezone);
+  console.log("converted Time Obj: " + convertedTime.toString());
+  console.log(
+    "FINAL RESULT: Timezone: " +
+      timezone +
+      " Time: " +
+      convertedTime.toLocaleString(DateTime.DATETIME_MED)
+  );
   return convertedTime.toLocaleString(DateTime.DATETIME_MED);
 };
